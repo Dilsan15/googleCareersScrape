24ac86ebb1c105735e6fb9018e7952e68017f27f
Dilshaan <dilshaan.sandhu6@gmail.com>
2023-01-08 13:04:11 -0700

    2023-01-08

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 from scraping import jobScraper
 
-# Number of forms needed to be scraped. Keep this number below 988 as this is linkedin's limit for loading more jobs
+# Number of jobs need to be scraped. Google often only has less then 100 job postings for most roles
 num_of_jobs_needed = 988
 
 # Timezone
@@ -18,7 +18,7 @@ website_page = "jobs/results/?distance=50&q=Machine%20Learning"
 driver_path = os.environ["DRIVER_PATH"]  # Todo("CHANGE THIS TO UR PATH! so it looks like driver_path = 'YOUR STRING' ")
 
 # time out needed between events, based on Wi-Fi and PC performance
-time_out = 3
+time_out = 1.5
 
 # Boolean which controls if the browser activities will be shown on screen on or not
 browser_visible = True
@@ -27,5 +27,5 @@ if __name__ == "__main__":
     JobScraper = jobScraper(basic_link, website_page, driver_path, num_of_jobs_needed, time_out, browser_visible,
                             scraping_timezone)
 
-"""Selenium was chosen as the web scraping tool as there was javascript involved in the website to display the job 
+"""Selenium was chosen as the web scraping tool as there was heavy javascript involved in the website to display the job 
 postings. """
--- scraping.py
+++ scraping.py
@@ -1,7 +1,7 @@
 import time
+from datetime import datetime
 
 import pandas as pd
-import selenium.common
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -14,46 +14,48 @@ class jobScraper:
     def __init__(self, basic_link, website_page, driver_path, links_needed, time_out, broswer_vis, timezone):
 
         self.links_needed = links_needed
-        self.full_link = basic_link + website_page
-        self.driver_path = driver_path
         self.time_out = time_out
         self.timezone = timezone
-        self.bSoup = None
 
         self.link_stored = list()
 
-        sel_service = Service(self.driver_path)
+        sel_service = Service(driver_path)
         option = webdriver.ChromeOptions()
 
         if not broswer_vis:
             option.add_argument("--headless")
 
         self.driver = webdriver.Chrome(service=sel_service, options=option)
-        self.driver.get(f'{self.full_link}')
+        self.driver.get(f'{basic_link + website_page}')
         self.getJobPostings()
         self.saveToCsv(self.getJobData())
 
     def getJobPostings(self):
-
+        """This function will scrape the website for the job postings and store them in a list"""
         while len(self.link_stored) < self.links_needed:
             time.sleep(self.time_out)
 
+            # Find all the job postings on the page
             self.link_stored.extend(
                 [hidLink.get_attribute("href") for hidLink in self.driver.find_elements(By.CLASS_NAME, "gc-card")])
 
-            print(self.link_stored)
-
+            # Scroll down and Click the next button
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
             try:
-                self.driver.find_element(By.XPATH,
-                                         '//*[@id="jump-content"]/div[1]/div/div[2]/main/div/div[2]/div/div/div/a[2]').click()
+                self.driver.find_elements(By.CLASS_NAME,
+                                          'gc-link--on-grey')[1].click()
 
-            except selenium.common.ElementNotInteractableException:
+            except:
+                # Only occurs if we run out pages to scrape
                 break
 
-            print(str(len(self.link_stored)) + " / " + str(self.links_needed))
+            # Remove any duplicate links
             self.link_stored = list(dict.fromkeys(self.link_stored))
+
+            # Indicate progress
+            print(str(len(self.link_stored)) + " / " + str(self.links_needed))
+
             time.sleep(self.time_out)
 
         if len(self.link_stored) >= self.links_needed:
@@ -62,7 +64,7 @@ class jobScraper:
         print("final " + str(len(self.link_stored)) + "/" + str(self.links_needed))
 
     def getJobData(self):
-
+        """This function will scrape the list of job postings and store the data in a list of dictionaries"""
         allJobData = list()
 
         for jobPost in self.link_stored:
@@ -71,13 +73,15 @@ class jobScraper:
 
             self.driver.get(jobPost)
             time.sleep(self.time_out)
-            self.bSoup = bs(self.driver.page_source, 'html.parser')
+            bSoup = bs(self.driver.page_source, 'html.parser')
 
-            jobData["Title"] = self.bSoup.find("h1", {"class": "gc-card__title"}).text
+            jobData["Title"] = bSoup.find("h1", {"class": "gc-card__title"}).text
+            jobData["Date-Scraped"] = f"{datetime.now()} {self.timezone}"
             jobData["URL"] = self.driver.current_url
 
             """Google Careers is not consistent with how they display locations. Leaving this as a comment for now since
-                is not 100 functioning
+                is not functioning 100% of the time. If we need this location data, I can try using a library to find locatoins 
+                in the text
             """
 
             # try:
@@ -103,10 +107,12 @@ class jobScraper:
             #     jobData["Office-Location"] = self.bSoup.find("div", itemprop="address").text
             #     jobData["Remote-Location"] = "N/A"
 
-            jobData["Company"] = self.bSoup.find("span", itemprop="name").text
+            jobData["Company"] = bSoup.find("span", itemprop="name").text
+
+            # Try getting job qualification, if it fails, set the value to "N/A" as it does not exist
 
             try:
-                job_qualifications = self.bSoup.find("div", itemprop="qualifications").findAll("ul")
+                job_qualifications = bSoup.find("div", itemprop="qualifications").findAll("ul")
                 jobData["Min-Qualifications"] = job_qualifications[0].text
                 jobData["Pref-Qualifications"] = job_qualifications[1].text
 
@@ -114,13 +120,12 @@ class jobScraper:
                 jobData["Min-Qualifications"] = "N/A"
                 jobData["Pref-Qualifications"] = "N/A"
 
-            jobData["Description"] = self.bSoup.find("div", itemprop="description").text
+            jobData["Description"] = bSoup.find("div", itemprop="description").text
 
             allJobData.append(jobData)
             print((str(self.link_stored.index(jobPost) + 1) + " / " + str(self.links_needed)))  # Indicates progress
 
         return allJobData
-
 
     def saveToCsv(self, data):
         """"Saves the data to a csv file and overwrites any previous data"""
